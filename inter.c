a16bf6745ff986a4732a695f970a5de814ae92e0
nxt-cy <unknowxd1780@gmail.com>
2024-12-16 21:54:03 +0530

    Add files via upload

---
+++ inter.c
@@ -0,0 +1,140 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+int i=1, j=0, no=0, tmpch=90;
+char str[100], left[15], right[15];
+
+void findopr();
+void explore();
+void fleft(int);
+void fright(int);
+ 
+struct exp {
+    int pos;
+    char op;
+    int isUnary;
+} k[15];
+
+void main() {
+    printf("\t\tINTERMEDIATE CODE GENERATION\n\n");
+    printf("Enter the Expression : ");
+    scanf("%s", str);
+    printf("The intermediate code:\n");
+    findopr();
+    explore();
+}
+
+void findopr() {
+    for (i = 0; str[i] != '\0'; i++) {
+        if (str[i] == '(' || str[i] == ')') {
+            k[j].pos = i;
+            k[j].isUnary=0;
+            k[j++].op = str[i];
+        }
+    }
+
+    for (i = 0; str[i] != '\0'; i++) {
+        if (str[i] == '-' && (i == 0 || (str[i-1] == '(' || str[i-1] == '+' || str[i-1] == '-' || str[i-1] == '*' || str[i-1] == '/'))) {
+            k[j].pos = i;
+            k[j].isUnary=1;
+            k[j++].op = '-';
+        }
+    }
+    
+    for (i = 0; str[i] != '\0'; i++) {
+        if (str[i] == '/') {
+            k[j].pos = i;
+            k[j].isUnary=0;
+            k[j++].op = '/';
+        }else if(str[i] == '*'){
+            k[j].pos = i;
+            k[j].isUnary=0;
+            k[j++].op = '*';   
+        }
+    }
+    
+    for (i = 0; str[i] != '\0'; i++) {
+        if (str[i] == '+') {
+            k[j].pos = i;
+            k[j].isUnary=0;
+            k[j++].op = '+';
+        }
+    }
+    
+    for (i = 0; str[i] != '\0'; i++) {
+        if (str[i] == '-') {
+            if (i == 0 || (str[i-1] == '(' || str[i-1] == '+' || str[i-1] == '-' || str[i-1] == '*' || str[i-1] == '/')) {
+                // Unary minus detected (after '(', '+', '-', '*', or '/')
+                continue;
+            }
+            k[j].pos = i;
+            k[j].isUnary=0;
+            k[j++].op = '-';
+        }
+    }
+}
+
+void explore() {
+    i = 0;
+    char finalTemp = '\0';  // To store the last temporary variable
+    while (k[i].op != '\0') {
+        if (k[i].op == '(' || k[i].op == ')') {
+            // Skip parentheses
+            i++;
+            continue;
+        }
+
+        if (k[i].isUnary) {
+            fright(k[i].pos);
+            str[k[i].pos] = tmpch--;
+            printf("\t%c := %c %s\t\t", str[k[i].pos], k[i].op, right);
+            finalTemp = str[k[i].pos];  // Store the last temp variable
+        } else {
+            fleft(k[i].pos);
+            fright(k[i].pos);
+            str[k[i].pos] = tmpch--;  // Replace the operator with temporary variable
+
+            // Print intermediate code
+            printf("\t%c := %s %c %s\t\t", str[k[i].pos], left, k[i].op, right);
+            finalTemp = str[k[i].pos];  // Store the last temp variable
+        }
+        printf("\n");
+        i++;
+    }
+
+    // Final assignment to original variable (e.g., b := Y)
+    printf("\t%c := %c\n", str[0], finalTemp);
+}
+
+
+
+void fleft(int x) {
+    int w = 0, flag = 0;
+    x--;
+    
+    while (x != -1 && str[x] != '+' && str[x] != '*' && str[x] != '=' && str[x] != '\0' && str[x] != '-' && str[x] != '/' && str[x] != ':') {
+        if (str[x] != '$' && flag == 0) {
+            left[w++] = str[x];
+            left[w] = '\0';
+            str[x] = '$'; // Mark the operand as processed
+            flag = 1;
+        }
+        x--;
+    }
+}
+
+void fright(int x) {
+    int w = 0, flag = 0;
+    x++;
+    
+    while (x != -1 && str[x] != '+' && str[x] != '*' && str[x] != '\0' && str[x] != '=' && str[x] != '-' && str[x] != '/' && str[x] != ':') {
+        if (str[x] != '$' && flag == 0) {
+            right[w++] = str[x];
+            right[w] = '\0';
+            str[x] = '$'; // Mark the operand as processed
+            flag = 1;
+        }
+        x++;
+    }
+}
